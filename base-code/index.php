a96a7f54f6e9a298670d2e2773273f104c09fa99
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-11-30 11:43:04 -0600

    Build header of page

--- base-code/index.php
+++ base-code/index.php
@@ -1,45 +1,75 @@
 <!DOCTYPE html>
 <html lang="en">
-	<!--begin::Head-->
-	<head>
-		<base href="" />
-		<title></title>
-		<meta charset="utf-8" />
-		<link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
-		<!--begin::Fonts(mandatory for all pages)-->
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
-		<!--end::Fonts-->
-		<!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
-		<link href="styles/uikit/plugins/global/plugins.bundle.css?v=<?php echo filemtime('styles/uikit/plugins/global/plugins.bundle.css') ?>" rel="stylesheet" type="text/css" />
-		<link href="styles/uikit/css/style.bundle.css?v=<?php echo filemtime('styles/uikit/css/style.bundle.css') ?>" rel="stylesheet" type="text/css" />
-		<!--end::Global Stylesheets Bundle-->
-		<!-- our custom styles -->
-		<link href="styles/main.css?v=<?php echo filemtime("styles/main.css"); ?>" rel="stylesheet" type="text/css" />
-		<link href="styles/raw-main.css?v=<?php echo filemtime("styles/raw-main.css"); ?>" rel="stylesheet" type="text/css" />
-		<!-- our custom styles end-->
-	</head>
-	<!--end::Head-->
-	<!--begin::Body-->
-	<body id="kt_app_body" data-kt-app-header-fixed-mobile="true" data-kt-app-sidebar-enabled="true" data-kt-app-sidebar-fixed="true" data-kt-app-sidebar-push-header="true" data-kt-app-sidebar-push-toolbar="true" data-kt-app-sidebar-push-footer="true" data-kt-app-sidebar-stacked="true" data-kt-app-toolbar-enabled="true" class="app-default">
+<!--begin::Head-->
 
-		<div id="app_container">
+<head>
+	<base href="" />
+	<title></title>
+	<meta charset="utf-8" />
+	<link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
+	<!--begin::Fonts(mandatory for all pages)-->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
+	<!--end::Fonts-->
+	<!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
+	<link href="styles/uikit/plugins/global/plugins.bundle.css?v=<?php echo filemtime('styles/uikit/plugins/global/plugins.bundle.css') ?>" rel="stylesheet" type="text/css" />
+	<link href="styles/uikit/css/style.bundle.css?v=<?php echo filemtime('styles/uikit/css/style.bundle.css') ?>" rel="stylesheet" type="text/css" />
+	<!--end::Global Stylesheets Bundle-->
+	<!-- our custom styles -->
+	<link href="styles/main.css?v=<?php echo filemtime("styles/main.css"); ?>" rel="stylesheet" type="text/css" />
+	<link href="styles/raw-main.css?v=<?php echo filemtime("styles/raw-main.css"); ?>" rel="stylesheet" type="text/css" />
+	<!-- our custom styles end-->
+</head>
+<!--end::Head-->
+<!--begin::Body-->
+
+<body id="kt_app_body" data-kt-app-header-fixed-mobile="true" data-kt-app-sidebar-enabled="true" data-kt-app-sidebar-fixed="true" data-kt-app-sidebar-push-header="true" data-kt-app-sidebar-push-toolbar="true" data-kt-app-sidebar-push-footer="true" data-kt-app-sidebar-stacked="true" data-kt-app-toolbar-enabled="true" class="app-default">
+
+	<div id="app_container">
 		<!-- Begin::your code -->
 
-		<div class="text-center mt-4 pt-5">
-			This page Works
-		</div>
+		<!-- Begin::Header -->
+		<header class="header">
+			<div class="row">
+				<div class="col border-end py-2 d-flex justify-content-start align-items-center">
+					<h1 class="px-4">Preview</h1>
+					<a class="link_approved py-2 px-4 rounded" href="#">
+						<span><i class="fa-solid fa-bars"></i></span>
+						Approved
+					</a>
+				</div>
+				<div class="col py-2">
+					<h1>Activity</h1>
+				</div>
+			</div>
+		</header>
+		<!-- End::Header -->
+
+		<!-- Begin::Main -->
+
+		<main class="carousel">
+			<div class="d-flex">
+				<button class="border-0 prev">
+					< </button>
+						<button class="border-0 next"> > </button>
+			</div>
+		</main>
+
+		<!-- End::Main -->
 
 		<!-- End::your code -->
-		</div>
+	</div>
 
-		<!--begin::Javascript-->
-		<script>var hostUrl = "scripts/lib/uikit/";</script>
-		<script src="scripts/lib/uikit/plugins/global/plugins.bundle.js?v=<?php echo filemtime('scripts/lib/uikit/plugins/global/plugins.bundle.js') ?>"></script>
-		<script src="scripts/lib/uikit/js/scripts.bundle.js?v=<?php echo filemtime('scripts/lib/uikit/js/scripts.bundle.js') ?>"></script>
-		<script src="scripts/lib/uikit/js/widgets.bundle.js?v=<?php echo filemtime('scripts/lib/uikit/js/widgets.bundle.js') ?>"></script>
+	<!--begin::Javascript-->
+	<script>
+		var hostUrl = "scripts/lib/uikit/";
+	</script>
+	<script src="scripts/lib/uikit/plugins/global/plugins.bundle.js?v=<?php echo filemtime('scripts/lib/uikit/plugins/global/plugins.bundle.js') ?>"></script>
+	<script src="scripts/lib/uikit/js/scripts.bundle.js?v=<?php echo filemtime('scripts/lib/uikit/js/scripts.bundle.js') ?>"></script>
+	<script src="scripts/lib/uikit/js/widgets.bundle.js?v=<?php echo filemtime('scripts/lib/uikit/js/widgets.bundle.js') ?>"></script>
 
-		<script src="scripts/lib/custom.js?v=<?php echo filemtime('scripts/lib/custom.js') ?>"></script>
-		<!--end::Javascript-->
-	</body>
-	<!--end::Body-->
+	<script src="scripts/lib/custom.js?v=<?php echo filemtime('scripts/lib/custom.js') ?>"></script>
+	<!--end::Javascript-->
+</body>
+<!--end::Body-->
+
 </html>
